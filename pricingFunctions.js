2edd00225c6368ec12090f980939e1ead7562957
Stephen Orr <stephen@fyresite.com>
2017-06-12 15:36:24 -0700

    Pushin for mike

---
+++ pricingFunctions.js
@@ -0,0 +1,14 @@
+class Print {
+  constructor() {
+    this.width = 0;
+    this.height = 0;
+  }
+
+  setHeight(height) {
+    this.height = height;
+  }
+
+  setWidth(width) {
+    this.width = width;
+  }
+}
